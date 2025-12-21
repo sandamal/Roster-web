b87ec061bdc720675f608ba415ba81e5177e9703
sandamal <sandamal0088@gmail.com>
2025-12-21 10:37:25 +0530

    Add files via upload
    Update for daily roster web install

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener("install", e => {
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => new Response("Offline"))
+  );
+});
